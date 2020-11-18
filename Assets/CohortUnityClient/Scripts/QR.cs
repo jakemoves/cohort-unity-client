b033044a43499038dea495c250a2db5339ccd5c1
Luke Garwood <luke_gar@hotmail.com>
2020-11-17 20:52:27 -0500

    added check url, success message, fixed inverted QR

--- Assets/CohortUnityClient/Scripts/QR.cs
+++ Assets/CohortUnityClient/Scripts/QR.cs
@@ -5,53 +5,110 @@ using UnityEngine;
 using ZXing;
 using ZXing.QrCode;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class QR : MonoBehaviour
 {
 
-  private WebCamTexture camTexture;
-  private Rect screenRect;
-  static public string QRresults;
-  Vector2 pivotPoint; 
+  private bool camAvailable;
+  private WebCamTexture backCam;
+  private Texture backgroundTexture;
+  private string QRresults;
 
+  public RawImage backgroundImage;
+  public AspectRatioFitter fit;
+  public GameObject success;
+  // Start is called before the first frame update
   void Start()
   {
-    screenRect = new Rect(0, 0, Screen.width, Screen.height);
+    backgroundTexture = backgroundImage.texture;
+    WebCamDevice[] devices = WebCamTexture.devices;
 
-    camTexture = new WebCamTexture();
-    camTexture.requestedHeight = Screen.height;
-    camTexture.requestedWidth = Screen.width;
-    if (camTexture != null)
+    if (devices.Length == 0)
     {
-      camTexture.Play();
+      Debug.Log("No camera present");
+      camAvailable = false;
+      return;
     }
+
+    for (int i = 0; i < devices.Length; i++)
+    {
+      if (!devices[i].isFrontFacing)
+      {
+        backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+
+      }
+
+    }
+
+    if (backCam == null)
+    {
+      Debug.Log("Unable to find back camera.");
+      return;
+    }
+
+    backCam.Play();
+    backgroundImage.texture = backCam;
+
+    camAvailable = true;
+
   }
 
-
-  void OnGUI()
+  // Update is called once per frame
+  void Update()
   {
-    //rotate image
-    pivotPoint = new Vector2(Screen.width / 2, Screen.height / 2);
-    GUIUtility.RotateAroundPivot(180, pivotPoint);
-    // drawing the camera on screen
+    if (!camAvailable)
+    {
+      return;
+    }
 
-    GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
-    // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+    float ratio = (float)backCam.width / (float)backCam.height;
+    fit.aspectRatio = ratio;
+
+    float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+    backgroundImage.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+    int orient = -backCam.videoRotationAngle;
+    backgroundImage.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
     try
     {
       IBarcodeReader barcodeReader = new BarcodeReader();
       // decode the current frame
-      var result = barcodeReader.Decode(camTexture.GetPixels32(),
-        camTexture.width, camTexture.height);
+      var result = barcodeReader.Decode(backCam.GetPixels32(),
+        backCam.width, backCam.height);
       if (result != null)
       {
-        Debug.Log("DECODED TEXT FROM QR: " +result.Text);
+        Debug.Log("DECODED TEXT FROM QR: " + result.Text);
         QRresults = result.Text;
-        PlayerPrefs.SetString("URL_from_QR", QRresults);
-        SceneManager.LoadScene("CohortDemoScene");
+        Uri uri = new Uri(QRresults);
+        if (UrlHasOccasion(uri))
+        {
+          PlayerPrefs.SetString("URL_from_QR", QRresults);
+          backCam.Stop();
+          success.SetActive(true);
+          SceneManager.LoadScene("CohortDemoScene");
+        }
       }
     }
     catch (Exception ex) { Debug.LogWarning(ex.Message); }
   }
 
+  bool UrlHasOccasion(Uri url)
+  {
+    string path = url.AbsolutePath;
+    string urlInput = "(occasion)";
+
+    // Instantiate the regular expression objects.
+    Regex compareUrl = new Regex(urlInput, RegexOptions.IgnoreCase);
+
+    // Match the regular expression pattern against our URL string.
+    Match matchUrl = compareUrl.Match(path);
+
+    return (matchUrl.Length > 0) ? true : false;
+  }
+
+
 }
+
