1d43104041a25a5318348e3ca3c66eee2338f7b4
M1Spots <nictoon@gmail.com>
2021-08-08 11:35:02 -0700

    SetTransformToDevice

--- Assets/CohortUnityClient/Scripts/Sensors/SampleAudioManipulation.cs
+++ Assets/CohortUnityClient/Scripts/Sensors/SampleAudioManipulation.cs
@@ -8,7 +8,13 @@ public class SampleAudioManipulation : MonoBehaviour
     // Start is called before the first frame update
     AudioSource source;
     public Dropdown selector;
+    public SensorManager sensorManager;
     int sel = 5;
+
+     
+    float lowPassFilterFactor = (1 / 60) / 1f;
+    Vector3 lowPassValue = Vector3.zero;
+
     void Start()
     {
         source = this.GetComponent<AudioSource>();
@@ -29,19 +35,25 @@ public class SampleAudioManipulation : MonoBehaviour
                 break;
 
             case 2:
-                ManipulateAudio(Input.gyro.gravity);
+                if (sensorManager != null) ManipulateAudio(sensorManager.GlobalAcceleration);
+                else ManipulateAudio(Vector3.zero);
                 break;
 
             case 3:
-                ManipulateAudio(Input.gyro.rotationRate);
+                ManipulateAudio(Input.gyro.gravity);
                 break;
 
             case 4:
-                ManipulateAudio(Input.gyro.rotationRateUnbiased);
+                ManipulateAudio(Input.gyro.rotationRate);
                 break;
 
             case 5:
-                ManipulateAudio(Input.gyro.attitude.eulerAngles);
+                ManipulateAudio(Input.gyro.rotationRateUnbiased);
+                break;
+
+            case 6:
+                if (sensorManager != null) ManipulateAudio(sensorManager.Attitude.eulerAngles);
+                else ManipulateAudio(Input.gyro.attitude.eulerAngles);
                 break;
 
             default:
@@ -52,8 +64,9 @@ public class SampleAudioManipulation : MonoBehaviour
     void ManipulateAudio(Vector3 vector)
     {
         if (source == null || vector == null) return;
-
-        source.panStereo = vector.normalized.z;
-        source.pitch = (vector.normalized.y * 1) + 1;
+        //lowPassValue = Vector3.Lerp(lowPassValue, vector.normalized, lowPassFilterFactor);
+        //source.panStereo = vector.normalized.x;
+        //source.pitch = lowPassValue.z;//(vector.normalized.z * Time.deltaTime * 9.81f) + 1;
+        source.pitch = vector.normalized.z + 1;
     }
 }
